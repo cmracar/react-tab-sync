b906818c5497b48ea5aab958b01d482c2f5e016e
Cemre Acar <cemreacar94@gmail.com>
2023-01-16 23:02:51 +0300

    broadcast channels created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
-import logo from './logo.svg';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [themeType, setThemeType] = useState('light')
+  const [username, setUsername] = useState()
+
+  const syncTheme = new BroadcastChannel("theme");
+  const syncLogin = new BroadcastChannel("login");
+
+  const switchTheme = () => {
+    if (themeType === 'light') {
+      setThemeType('dark')
+      syncTheme.postMessage('dark');
+    } else {
+      setThemeType('light')
+      syncTheme.postMessage('light');
+    }
+  }
+
+  const loggedIn = (state) => {
+    if (state) {
+      syncLogin.postMessage(true)
+      setIsLoggedIn(true)
+    } else {
+      syncLogin.postMessage(false)
+      setIsLoggedIn(false)
+    }
+  }
+
+  useEffect(() => {
+    syncTheme.onmessage = channel => {
+      console.log(channel)
+      setThemeType(channel.data)
+    }
+
+    return () => {
+      syncTheme.close()
+    }
+  }, [syncTheme, switchTheme])
+
+  useEffect(() => {
+    syncLogin.onmessage = channel => {
+      console.log(channel)
+      setIsLoggedIn(channel.data)
+    }
+
+    return () => {
+      syncLogin.close()
+    }
+  }, [syncLogin, loggedIn])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className={themeType === 'light' ? 'light-theme' : 'dark-theme'}>
+        <h1>Welcome Sample Website</h1>
+        {
+          isLoggedIn ?
+            <>
+              <p>Hi <b>{username}</b>,</p>
+              <p>You are logged in!</p>
+              <p>Change Your Mode:
+                {themeType === 'light' ?
+                  <button className='p-5 m-5' onClick={() => switchTheme()}>
+                    Switch Dark Mode
+                  </button>
+                  :
+                  <button className='p-5 m-5' onClick={() => switchTheme()}>
+                    Switch Light Mode
+                  </button>
+                }
+              </p>
+              <button className='p-5 m-5' onClick={() => loggedIn(false)}>Logout</button>
+            </>
+
+            :
+
+            <>
+              <label>
+                <h4 className='m-5'>Login</h4>
+                <input type='text' name='username' placeholder='Username' className='p-5' onChange={(e) => setUsername(e.target.value)} />
+              </label>
+              <button className='p-5 m-5' onClick={() => loggedIn(true)}>Login</button>
+            </>
+        }
+
+      </div>
+    </>
   );
 }
 
